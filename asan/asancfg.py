e5b1f03517a7535179a92a3701d319bfd9742a3d
Nirmal Bhujel <qwertyboy08@gmail.com>
2024-06-21 20:31:22 +0530

    Update asancfg.py

--- asan/asancfg.py
+++ asan/asancfg.py
@@ -7,7 +7,7 @@ project_id='1127916913032527'
 #access token
 #this acess token is sufficient to login to asana and fetch data from a particular account
 
-access_token="0/3e2def2173a11c8e674c8ee1a9fffbc0"
+access_token="0/3e2def2173a11c8e674c8ee1a9ff**c0"
 
 
 ##workspaces
